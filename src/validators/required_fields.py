ada4d7284338ab4208c903d650b513b33b98a8f8
JavierRichart <lopezrichart@gmail.com>
2026-07-26 15:41:20 +0200

    errors in validate

--- src/validators/required_fields.py
+++ src/validators/required_fields.py
@@ -9,4 +9,21 @@ class RequiredFieldsValidator(BaseValidator):
         self.required_fields = required_fields
 
     def validate(self, dataframe: pd.DataFrame) -> list[ValidationResult]:
-        return[]
+        errors = []
+
+        for _, row in dataframe.iterrows():
+            for field in self.required_fields:
+                if pd.isna(row[field]) or row[field]=="":
+                    errors.append(
+                        ValidationResult(
+                            closure_id=row["closure_id"],
+                            validation_code="REQUIRED_FIELD",
+                            validation_name="Campo obligatorio vacío",
+                            severity="ERROR",
+                            field=field,
+                            current_value=row[field],
+                            expected_value="Valor obligatorio",
+                            message=f"El campo '{field}' está vacío.",
+                        )
+                    )
+        return errors
