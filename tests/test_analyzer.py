c672271ecc99ef89c89475aef27f49380f2013a6
JavierRichart <lopezrichart@gmail.com>
2026-07-27 20:15:41 +0200

    Adding commodity validator to test

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -2,24 +2,27 @@ import pandas as pd
 
 from src.analyzer import run_validations
 from src.validators.required_fields import RequiredFieldsValidator
+from src.validators.commodity import CommodityValidator
 
 
-def test_run_validations():
+def test_run_multiple_validations():
     dataframe = pd.DataFrame(
         [
             {
                 "closure_id": 1,
                 "book": "",
-                "commodity": "GAS",
+                "commodity": "WATER",
             }
         ]
     )
 
     validators = [
-        RequiredFieldsValidator(["book", "commodity"])
+        RequiredFieldsValidator(["book", "commodity"]),
+        CommodityValidator(["GAS", "POWER"]),
     ]
 
     errors = run_validations(dataframe, validators)
 
-    assert len(errors) == 1
+    assert len(errors) == 2
     assert errors[0].validation_code == "REQUIRED_FIELD"
+    assert errors[1].validation_code == "INVALID_COMMODITY"
